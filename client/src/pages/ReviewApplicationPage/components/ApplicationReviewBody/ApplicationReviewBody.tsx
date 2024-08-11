da014035da40451b325989d95d18b3e850a200f7
Fabian Emilius <fabian.emilius@tum.de>
2024-08-12 01:48:35 +0200

    Scroll to top when switching selected application

--- client/src/pages/ReviewApplicationPage/components/ApplicationReviewBody/ApplicationReviewBody.tsx
+++ client/src/pages/ReviewApplicationPage/components/ApplicationReviewBody/ApplicationReviewBody.tsx
@@ -1,8 +1,9 @@
 import ApplicationData from '../../../../components/ApplicationData/ApplicationData'
 import ApplicationReviewForm from '../../../../components/ApplicationReviewForm/ApplicationReviewForm'
 import { Divider, Stack } from '@mantine/core'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { IApplication } from '../../../../requests/responses/application'
+import { useWindowScroll } from '@mantine/hooks'
 
 interface IApplicationReviewBodyProps {
   application: IApplication
@@ -11,6 +12,12 @@ interface IApplicationReviewBodyProps {
 
 const ApplicationReviewBody = (props: IApplicationReviewBodyProps) => {
   const { application, onChange } = props
+
+  const [, scrollTo] = useWindowScroll()
+
+  useEffect(() => {
+    scrollTo({ y: 0 })
+  }, [application.applicationId])
 
   return (
     <Stack>
