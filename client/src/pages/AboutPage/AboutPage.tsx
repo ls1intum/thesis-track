3b9453104c2542ba19daf0cf62716e2296e45849
Fabian Emilius <fabian.emilius@tum.de>
2024-11-22 00:44:44 +0100

    Update about page

--- client/src/pages/AboutPage/AboutPage.tsx
+++ client/src/pages/AboutPage/AboutPage.tsx
@@ -11,12 +11,14 @@ const AboutPage = () => {
       <Stack>
         <Title>ThesisTrack</Title>
         <Text>
-          ThesisTrack is an{' '}
-          <Anchor href='https://github.com/ls1intum/thesis-track' target='_blank' rel='noreferrer'>
-            open source
-          </Anchor>{' '}
-          web application that integrates the complete thesis lifecycle for theses supervised at a
-          chair. It should help advisors and supervisors to save time on reoccurring processes.
+          ThesisTrack is a web-based thesis management system designed to streamline the thesis
+          process in academic institutions by integrating essential stages into a single platform.
+          Developed to address challenges in managing large volumes of theses, it facilitates
+          seamless interactions between students, advisors, and supervisors. Key features include a
+          centralized application process, guided workflows for thesis writing, automated
+          notifications, and a comprehensive Gantt chart for tracking progress. By consolidating
+          communication, feedback, and file management, ThesisTrack enhances transparency, reduces
+          administrative burdens, and fosters efficient thesis supervision and assessment.
         </Text>
         <Title order={3}>Contributors</Title>
         <List>
@@ -32,6 +34,16 @@ const AboutPage = () => {
           </List.Item>
         </List>
         <Title order={3}>Features</Title>
+        <Text>
+          The following flowchart diagram provides a visual overview of the thesis processes
+          implemented in ThesisTrack. These diagram illustrates the step-by-step workflows involved,
+          from thesis topic selection and application submission to the final grading and completion
+          stages. It highlights key actions, decision points, and interactions between students,
+          advisors, and supervisors, clarifying how tasks are sequenced and managed within the
+          system. These flowcharts offer a quick reference for understanding how each role engages
+          in the thesis process, ensuring transparency and consistency in task progression and
+          responsibilities across different stages.
+        </Text>
         <Center>
           <Image src={flowchart} style={{ maxWidth: '600px' }} />
         </Center>
